611312b6be7ba3aac57a829099728b63c305f830
Marcia Leite <marciasilvaleite@gmail.com>
2025-01-07 11:06:49 -0500

    Add contact page

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -5,11 +5,23 @@ const contactLink = document.getElementById('contact-link');
 const homeDiv = document.getElementById('home');
 const aboutDiv = document.getElementById('about');
 const contactDiv = document.getElementById('contact');
+const navbarSearch = document.getElementById('navbar-search');
 
 function showDiv(selectedDiv) {
-    const divs = [homeDiv, aboutDiv, contactDiv];
+    const divs = [homeDiv, aboutDiv, contactDiv, navbarSearch];
     divs.forEach(div => {
-        div.style.display = div === selectedDiv ? 'block' : 'none';
+        if(div == navbarSearch) {
+            if(selectedDiv == homeDiv ) {
+                div.style.display = 'block';
+            } else {
+                div.style.display = 'none';
+                const navbarLinks = document.getElementsByClassName('navbar-links');
+                navbarLinks.style.marginRight = "50%";
+            }
+        } else {            
+            div.style.display = div === selectedDiv ? 'block' : 'none';
+        }        
+        
     });
 }
 
